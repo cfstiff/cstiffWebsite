6d1c58a5599b42b7bc87dab2c14e65da8948da51
Coco Stiff <cstiff@g.hmc.edu>
2018-08-11 11:05:46 -0500

    reverting node update

--- deploy.sh
+++ deploy.sh
@@ -35,16 +35,13 @@ fi
 
 for DEPLOY_SUBDOMAIN_UNFORMATTED in "${DEPLOY_SUBDOMAIN_UNFORMATTED_LIST[@]}"
 do
-  echo $DEPLOY_SUBDOMAIN_UNFORMATTED
   # replaces "/" or "." with "-"
   # sed -r is only supported in linux, ref http://stackoverflow.com/a/2871217/689223
   # Domain names follow the RFC1123 spec [a-Z] [0-9] [-]
   # The length is limited to 253 characters
   # https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax
   DEPLOY_SUBDOMAIN=`echo "$DEPLOY_SUBDOMAIN_UNFORMATTED" | sed -r 's/[\/|\.]+/\-/g'`
-  echo $DEPLOY_SUBDOMAIN
   DEPLOY_DOMAIN=http://${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.cstiff.com
-  echo $DEPLOY_DOMAIN
   surge --project ${DEPLOY_PATH} --domain $DEPLOY_DOMAIN;
   if [ "$TRAVIS_PULL_REQUEST" != "false" ]
   then
