64ea05b4a5f22e995981f3d8c24e4341631659c1
Coco Stiff <cstiff@g.hmc.edu>
2018-08-11 09:10:19 -0500

    updating subdomain

--- deploy.sh
+++ deploy.sh
@@ -42,7 +42,8 @@ do
   # The length is limited to 253 characters
   # https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax
   DEPLOY_SUBDOMAIN=`echo "$DEPLOY_SUBDOMAIN_UNFORMATTED" | sed -r 's/[\/|\.]+/\-/g'`
-  DEPLOY_DOMAIN=https://${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.cstiff.com
+  DEPLOY_DOMAIN=http://${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.cstiff.com
+  console.log(DEPLOY_DOMAIN);
   surge --project ${DEPLOY_PATH} --domain $DEPLOY_DOMAIN;
   if [ "$TRAVIS_PULL_REQUEST" != "false" ]
   then
